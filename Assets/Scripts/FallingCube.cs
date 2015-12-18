73f67653f675440dfbcf370ef1706415eb44a63c
thomja <thomja888@hotmail.com>
2015-12-18 13:15:03 +0100

    Fixed floor
    Fixed floor on the map to support the falling cube script.

--- Assets/Scripts/FallingCube.cs
+++ Assets/Scripts/FallingCube.cs
@@ -10,17 +10,12 @@ public class FallingCube : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		allBodys = floorParent.GetComponentsInChildren<Rigidbody>();
+		startFloorFall();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
-	}
-
-	void OnTriggerEnter(Collider Col){
-		if(Col.gameObject.tag == "Eater"){
-			this.gameObject.GetComponent<Rigidbody>().useGravity = true;
-		}
 	}
 
 	public void startFloorFall(){
